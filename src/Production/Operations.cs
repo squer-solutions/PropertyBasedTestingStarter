28f3915e1813d150c78eb381791609bbf1afc0a8
Paul Rohorzka <paul.rohorzka@squer.io>
2023-09-26 23:54:49 +0200

    Add(x, 0) == Add(0, x)

--- src/Production/Operations.cs
+++ src/Production/Operations.cs
@@ -2,5 +2,10 @@
 
 public static class Operations
 {
-    public static int Add(int a, int b) => 0;
+    public static int Add(int a, int b)
+    {
+        if (b == 0) return a;
+        if (a == 0) return b;
+        return 0;
+    }
 }
--- test/Tests/AddTests.cs
+++ test/Tests/AddTests.cs
@@ -1,12 +1,21 @@
 using FluentAssertions;
 using FsCheck;
 using FsCheck.Xunit;
+using Xunit.Abstractions;
 using static Squer.Workshops.PropertyBasedTesting.Production.Operations;
 
 namespace Tests;
 
 public class AddTests
+
 {
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public AddTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
     [Fact]
     public void ZeroPlusZeroIsZero()
     {
@@ -17,6 +26,13 @@ public class AddTests
     public Property AddIsCommutative(int x, int y)
     {
         return (Add(x, y) == Add(y, x))
+            .ToProperty();
+    }
+
+    [Property]
+    public Property ZeroIsIdentity(int x)
+    {
+        return (Add(x, 0) == x)
             .ToProperty();
     }
 }
