c96f0d22b9633590b81c4bb49a66f619e479f145
Paul Rohorzka <paul.rohorzka@squer.io>
2023-09-27 00:28:12 +0200

    more properties

--- src/Production/Operations.cs
+++ src/Production/Operations.cs
@@ -4,8 +4,6 @@ public static class Operations
 {
     public static int Add(int a, int b)
     {
-        if (b == 0) return a;
-        if (a == 0) return b;
-        return 0;
+        return a + b;
     }
 }
--- test/Tests/AddTests.cs
+++ test/Tests/AddTests.cs
@@ -35,4 +35,28 @@ public class AddTests
         return (Add(x, 0) == x)
             .ToProperty();
     }
+
+    [Property]
+    public Property AddSupportsAssociation(int x, int y, int z)
+    {
+        _testOutputHelper.WriteLine($"{x},{y},{z}");
+        return (Add(x, Add(y, z)) == Add(Add(x, y), z))
+            .ToProperty();
+    }
+
+    [Property]
+    public Property AddingOppositeNumbersIsZero(int x)
+    {
+        _testOutputHelper.WriteLine($"{x}");
+        return (Add(x, -x) == 0)
+            .ToProperty();
+    }
+
+    [Property]
+    public Property SumOfOppositeNumbers(int x, int y)
+    {
+        _testOutputHelper.WriteLine($"{x},{y}");
+        return (-Add(x, y) == Add(-x, -y))
+            .ToProperty();
+    }
 }
